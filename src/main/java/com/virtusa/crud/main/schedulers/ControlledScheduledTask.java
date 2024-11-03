c3d2752ec841ed005575f9bc752cd556ee6e80b9
srinivaspanchal <vadlasrinivas01@gmail.com>
2024-11-03 20:30:36 +0530

    addedd this classControlledScheduledTask

--- src/main/java/com/virtusa/crud/main/schedulers/ControlledScheduledTask.java
+++ src/main/java/com/virtusa/crud/main/schedulers/ControlledScheduledTask.java
@@ -1,15 +1,24 @@
 package com.virtusa.crud.main.schedulers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.virtusa.crud.main.entity.Product;
+import com.virtusa.crud.main.services.ProductService;
 
 @Component
 public class ControlledScheduledTask {
 
 	private boolean enabled = true;
+	@Autowired
+	private ProductService service;
 
 	 @Scheduled(fixedRate = 5000) //  5 seconds
 	    public void performTask() {
+		 
+		 Product product= new Product();
+		 service.saveProduct(product);
 	        if (enabled) {
 	            System.out.println("Controlled Task executed at " + java.time.LocalDateTime.now());
 	        }
